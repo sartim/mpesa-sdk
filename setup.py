62d947c5a3e0f3ed7521fb606a17a87245c134e0
Tim <sarrtim@gmail.com>
2018-06-04 12:57:23 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='M-PESA API SDK',
+      version='0.1',
+      description='M-Pesa API SDK',
+      url='https://github.com/sartim/mpesa-python-sdk',
+      author='sartim',
+      author_email='sarrtim@gmail.com',
+      license='MIT',
+      packages=['mpesa'],
+      install_requires=[
+          'requests',
+      ],
+      dependency_links=['https://github.com/sartim/mpesa-python-sdk/master#egg=package-1.0'],
+      zip_safe=False)
