8685a45954c1207ef085931572cc6c4c367fa2b3
Tim <sarrtim@gmail.com>
2018-06-09 20:45:32 +0300

    Rename file and update comments

--- mpesa/gateway.py
+++ mpesa/gateway.py
@@ -8,7 +8,6 @@ response returned is a response object from (python requests).
 From this object you can retrieve the response parameters
 as text or json.
 """
-from __future__ import unicode_literals
 
 import os
 import logging
@@ -21,7 +20,7 @@ from urls import URL
 logger = logging.getLogger()
 
 
-class Gateway:
+class Mpesa:
 
     def __init__(self, access_token, env=os.environ.get('ENV'), version="v1", timeout=None):
         self.headers = {"Authorization": "Bearer %s" % access_token}
@@ -30,16 +29,25 @@ class Gateway:
         self.timeout = timeout
 
     def make_request(self, url, payload, method):
-        """Invoke url and return a python request object"""
+        """
+        Invoke url and return a python request object.
+        :param url:
+        :param payload:
+        :param method:
+        :return response (obj):
+        """
         if self.timeout:
             return requests.request(method, url, headers=self.headers, json=payload, timeout=self.timeout)
         else:
             return requests.request(method, url, headers=self.headers, json=payload)
 
     def b2b_payment_request(self, data):
-        """Mpesa Transaction from one company to another.
-
+        """
+        B2B Payment request.
+        Mpesa Transaction from one company to another.
         https://developer.safaricom.co.ke/b2b/apis/post/paymentrequest
+        :param data:
+        :return response (json):
         """
         expected_keys = ["Initiator", "SecurityCredential",
                          "CommandID", "SenderIdentifierType",
@@ -55,14 +63,13 @@ class Gateway:
         return response
 
     def b2c_payment_request(self, data):
-
         """
-        B2B
+        B2C Payment Request.
         Mpesa Transaction from company to client.
-
         https://developer.safaricom.co.ke/b2c/apis/post/paymentrequest
+        :param data:
+        :return response (json):
         """
-
         expected_keys = ["InitiatorName", "SecurityCredential",
                          "CommandID", "Amount", "PartyA",
                          "PartyB", "Remarks", "QueueTimeOutURL",
@@ -76,10 +83,14 @@ class Gateway:
         return response
 
     def c2b_register_url(self, data):
-
-        """Use this API to register validation and confirmation URLs on M-Pesa.
-
+        """
+        C2B Register Url.
+        Use this API to register validation and confirmation URLs on M-Pesa.
+        NOTE: This should be registered once. To update the urls registered contact
+        Safaricom which will take some time to be propagated.
         https://developer.safaricom.co.ke/c2b/apis/post/registerurl
+        :param data:
+        :return response (json):
         """
         expected_keys = ["ShortCode", "ResponseType",
                          "ConfirmationURL", "ValidationURL"]
@@ -92,15 +103,13 @@ class Gateway:
         return response
 
     def c2b_simulate_transaction(self, data):
-
         """
-        C2B Simulate Transaction
-
+        C2B Simulate Transaction.
         Use this API to simulate a C2B transaction.
-
         https://developer.safaricom.co.ke/c2b/apis/post/simulate
+        :param data:
+        :return response (json):
         """
-
         expected_keys = ["ShortCode", "Amount",
                          "Msisdn"]
         payload = process_data(expected_keys, data)
@@ -113,12 +122,13 @@ class Gateway:
         return response
 
     def transation_status_request(self, data):
-
-        """Use this API to check the status of transaction.
-
-        https://developer.safaricom.co.ke/transaction-status/apis/post/query
         """
-
+        Transaction Status Request.
+        Use this API to check the status of transaction.
+        https://developer.safaricom.co.ke/transaction-status/apis/post/query
+        :param data:
+        :return response (json):
+        """
         expected_keys = ["Initiator", "SecurityCredential",
                          "TransactionID", "PartyA",
                          "ResultURL", "QueueTimeOutURL",
@@ -131,16 +141,16 @@ class Gateway:
         if r.status_code != 200:
             logger.error("Transaction status request has not been completed")
         response = r.json()
-
         return response
 
     def account_balance_request(self, data):
-
-        """Use this API to enquire the balance on an M-Pesa BuyGoods (Till Number).
-
-        https://developer.safaricom.co.ke/account-balance/apis/post/query
         """
-
+        Account Balance Request.
+        Use this API to enquire the balance on an M-Pesa BuyGoods (Till Number).
+        https://developer.safaricom.co.ke/account-balance/apis/post/query
+        :param data:
+        :return response (json):
+        """
         expected_keys = ["Initiator", "SecurityCredential", "PartyA",
                          "Remarks", "QueueTimeOutURL", "ResultURL"]
         payload = process_data(expected_keys, data)
@@ -154,12 +164,13 @@ class Gateway:
         return response
 
     def reversal_request(self, data):
-
-        """Use this API for reversal transaction.
-
-        https://developer.safaricom.co.ke/reversal/apis/post/request
         """
-
+        Reversal Request.
+        Use this API for reversal transaction.
+        https://developer.safaricom.co.ke/reversal/apis/post/request
+        :param data:
+        :return response (json):
+        """
         expected_keys = ["Initiator", "SecurityCredential",
                          "TransactionID", "Amount",
                          "ReceiverParty", "ResultURL",
@@ -175,12 +186,13 @@ class Gateway:
         return response
 
     def lipa_na_mpesa_online_query(self, data):
-
-        """For Lipa Na M-Pesa online payment using STK Push.
-
-        https://developer.safaricom.co.ke/lipa-na-m-pesa-online/apis/post/stkpushquery/v1/query
         """
-
+        Lipa na M-Pesa online query.
+        For Lipa Na M-Pesa online payment using STK Push.
+        https://developer.safaricom.co.ke/lipa-na-m-pesa-online/apis/post/stkpushquery/v1/query
+        :param data:
+        :return response (json):
+        """
         expected_keys = ['BusinessShortCode', 'Password', 'Timestamp', 'CheckoutRequestID']
         payload = process_data(expected_keys, data)
         payload["Timestamp"] = generate_timestamp()
@@ -189,18 +201,17 @@ class Gateway:
         if r.status_code != 200:
             logger.error("M-Pesa express query request has not been completed")
         response = r.json()
-
         return response
 
     def lipa_na_mpesa_online_payment(self, data):
-
-        """For Lipa Na M-Pesa online payment using STK Push.
-
-        Use this API to initiate online payment on behalf of a customer.
-
-        https://developer.safaricom.co.ke/lipa-na-m-pesa-online/apis/post/stkpush/v1/processrequest
         """
-
+        Lipa na M-Pesa online payment
+        For Lipa Na M-Pesa online payment using STK Push.
+        Use this API to initiate online payment on behalf of a customer.
+        https://developer.safaricom.co.ke/lipa-na-m-pesa-online/apis/post/stkpush/v1/processrequest
+        :param data:
+        :return response (json):
+        """
         expected_keys = ["BusinessShortCode", "Password",
                          "Amount", "PartyA", "PartyB",
                          "PhoneNumber", "CallBackURL",
@@ -217,13 +228,18 @@ class Gateway:
 
 
 def oauth_generate_token(consumer_key, consumer_secret, grant_type="client_credentials", env="sandbox", version="v1"):
-
-    """Authenticate your app and return an OAuth access token.
-
-       This token gives you time bound access token to call allowed APIs.
-       NOTE: The OAuth access token expires after an hour (3600 seconds),
-             after which, you will need to generate another access token
-             so you need to keep track of this.
+    """
+    Authenticate your app and return an OAuth access token.
+    This token gives you time bound access token to call allowed APIs.
+    NOTE: The OAuth access token expires after an hour (3600 seconds),
+    after which, you will need to generate another access token so you
+    need to keep track of this.
+    :param consumer_key:
+    :param consumer_secret:
+    :param grant_type:
+    :param env:
+    :param version:
+    :return response:
     """
     response = requests.get(URL[env][version]["oauth_generate_token"],
                         params=dict(grant_type=grant_type),
@@ -238,20 +254,32 @@ def oauth_generate_token(consumer_key, consumer_secret, grant_type="client_crede
 
 
 def encode_password(shortcode, passkey, timestamp):
-    """Generate and return a base64 encoded password for online access.
+    """
+    Generate and return a base64 encoded password for online access.
+    :param shortcode:
+    :param passkey:
+    :param timestamp:
+    :return base64 encoded password:
     """
     return base64.encode(shortcode + passkey + timestamp)
 
 
 def generate_timestamp():
-    """Return the current timestamp formated as YYYYMMDDHHMMSS"""
+    """
+    Return the current timestamp formated as YYYYMMDDHHMMSS
+    :return current timestamp:
+    """
     return datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
 
 
 def process_data(expected_keys, data):
-    """Check for any expected but missing keyword arguments
-       and raise a TypeError else return the keywords arguments
-       repackaged in a dictionary i.e the payload.
+    """
+    Check for any expected but missing keyword arguments
+    and raise a TypeError else return the keywords arguments
+    repackaged in a dictionary i.e the payload.
+    :param expected_keys:
+    :param data:
+    :return payload:
     """
     payload = {}
     for key in expected_keys:
