67fda276269e29104b116c82a712460b9c5c9995
Tim <sarrtim@gmail.com>
2020-03-10 16:30:41 +0300

    Update sdk name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='mpesa-api-sdk',
+setup(name='mpesa-sdk',
       version='1.0.0',
       description='Mpesa API SDK',
       long_description=long_description,
