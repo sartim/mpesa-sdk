c03d9aa3b1cdee5be2f482581478b790f1d723a0
Tim <sarrtim@gmail.com>
2020-03-10 19:03:37 +0300

    Update generate token unittest

--- tests/test_generate_token.py
+++ tests/test_generate_token.py
@@ -6,10 +6,11 @@ from tests.base import BaseTest
 
 class TestGenerateToken(BaseTest):
     def test_generate_token(self):
-        result = oauth_generate_token(
+        result, status_code = oauth_generate_token(
             consumer_key=os.environ.get("CONSUMER_KEY"),
             consumer_secret=os.environ.get("CONSUMER_SECRET"),
             grant_type="client_credentials",
             env="sandbox")
         assert result is not None
+        assert status_code == 200
         assert "access_token" in result.json()
