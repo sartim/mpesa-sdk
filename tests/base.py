b0ea9654f0652badcec89fa9b1f1fbac937c1ad5
Tim <sarrtim@gmail.com>
2020-04-01 20:19:44 +0300

    Update base setup class

--- tests/base.py
+++ tests/base.py
@@ -9,9 +9,11 @@ load_dotenv()
 class BaseTest:
     @classmethod
     def setup_class(cls):
+        cls.consumer_key = os.environ.get("CONSUMER_KEY")
+        cls.consumer_secret = os.environ.get("CONSUMER_SECRET")
         access_token = oauth_generate_token(
-            consumer_key=os.environ.get("CONSUMER_KEY"),
-            consumer_secret=os.environ.get("CONSUMER_SECRET"),
+            consumer_key=cls.consumer_key,
+            consumer_secret=cls.consumer_secret,
             grant_type="client_credentials",
             env="sandbox")
         cls.instance = Mpesa(access_token, env="sandbox")
