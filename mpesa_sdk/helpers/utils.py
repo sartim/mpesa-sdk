1618d0a61b26b6292c124f7d99aedebced3986c3
Tim <sarrtim@gmail.com>
2020-04-01 21:27:13 +0300

    Add helper for converting datetime object to integer

--- mpesa_sdk/helpers/utils.py
+++ mpesa_sdk/helpers/utils.py
@@ -22,3 +22,8 @@ def current_timestamp(ms=True):
     if ms:
         return int((to_second + 0.5) * 1000)
     return to_second
+
+
+def convert_datetime_to_int(dt_time):
+    """Convert date time object to integer"""
+    return 10000 * dt_time.year + 100 * dt_time.month + dt_time.day + dt_time.second
