f1089065e41f863879ea4519cee5fddc40856b6a
Tim <sarrtim@gmail.com>
2020-04-01 20:03:07 +0300

    Add current timestamp helper

--- mpesa_sdk/helpers/utils.py
+++ mpesa_sdk/helpers/utils.py
@@ -1,5 +1,6 @@
 import logging
 import base64
+import time
 
 
 def encode_string(txt):
@@ -11,3 +12,13 @@ def encode_string(txt):
     else:
         return encoded_string
     return False
+
+
+def current_timestamp(ms=True):
+    """Retrieves up to milliseconds when set to true.
+    Otherwise return to second
+    """
+    to_second = time.time()
+    if ms:
+        return int((to_second + 0.5) * 1000)
+    return to_second
