f8eb85f031feca079c77abd26a3648073f3f62cb
Tim <sarrtim@gmail.com>
2020-04-01 20:21:57 +0300

    Refactor to use instance variable values parsed on setup

--- tests/test_lipa_na_mpesa_online_query.py
+++ tests/test_lipa_na_mpesa_online_query.py
@@ -7,14 +7,10 @@ from tests.base import BaseTest
 
 class TestLipaNaMpesaOnlineQuery(BaseTest):
     def test_successful_lipa_na_mpesa_online_query(self):
-        short_code = os.environ.get("SAFARICOM_SHORT_CODE")
-        current_timestamp =  utils.current_timestamp
-        password = utils.encode_string(
-            short_code+self.consumer_key+current_timestamp)
         data = {
-            "BusinessShortCode": short_code,
-            "Password": password,
-            "Timestamp": current_timestamp,
+            "BusinessShortCode": self.short_code,
+            "Password": self.password,
+            "Timestamp": self.current_timestamp,
             "CheckoutRequestID": str(uuid.uuid4())
         }
         body, status_code = self.instance.lipa_na_mpesa_online_query(data)
