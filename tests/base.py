3519399d0f980b414560a15d172cad1f5e169fa2
Tim <sarrtim@gmail.com>
2020-04-05 13:44:18 +0300

    Decode bytes to string on password

--- tests/base.py
+++ tests/base.py
@@ -14,15 +14,16 @@ class BaseTest:
         current_date_time = datetime.datetime.today()
         cls.consumer_key = os.environ.get("CONSUMER_KEY")
         cls.consumer_secret = os.environ.get("CONSUMER_SECRET")
-        cls.short_code = os.environ.get("SAFARICOM_SHORT_CODE")
+        cls.short_code = int(os.environ.get("SAFARICOM_SHORT_CODE"))
         cls.mobile_number = os.environ.get("SAFARICOM_TEST_MOBILE_NUMBER")
-        cls.current_timestamp = utils.convert_datetime_to_int(current_date_time)
+        cls.current_timestamp = str(utils.convert_datetime_to_int(current_date_time))
         cls.callback_url = os.environ.get("SAFARICOM_CALL_BACK_URL")
         cls.password = utils.encode_string(
-            cls.short_code + cls.consumer_key + cls.current_timestamp)
-        access_token = oauth_generate_token(
+            str(cls.short_code) + cls.consumer_key + str(cls.current_timestamp))\
+            .decode()
+        result, status_code = oauth_generate_token(
             consumer_key=cls.consumer_key,
             consumer_secret=cls.consumer_secret,
             grant_type="client_credentials",
             env="sandbox")
-        cls.instance = Mpesa(access_token, env="sandbox")
+        cls.instance = Mpesa(result["access_token"], env="sandbox")
