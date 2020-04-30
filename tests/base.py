6b9c0660c7633d5471581f8756d6cd4fc96a6f3f
Tim <sarrtim@gmail.com>
2020-04-30 19:01:24 +0300

    Update base test

--- tests/base.py
+++ tests/base.py
@@ -12,14 +12,16 @@ class BaseTest:
     @classmethod
     def setup_class(cls):
         current_date_time = datetime.datetime.today()
+        cls.passkey = os.environ.get("SAFARICOM_LIPA_NA_MPESA_PASSKEY")
         cls.consumer_key = os.environ.get("CONSUMER_KEY")
         cls.consumer_secret = os.environ.get("CONSUMER_SECRET")
         cls.short_code = int(os.environ.get("SAFARICOM_SHORT_CODE"))
         cls.mobile_number = os.environ.get("SAFARICOM_TEST_MOBILE_NUMBER")
         cls.current_timestamp = str(utils.convert_datetime_to_int(current_date_time))
+        cls.order_id = "11223344"
         cls.callback_url = os.environ.get("SAFARICOM_CALL_BACK_URL")
         cls.password = utils.encode_string(
-            str(cls.short_code) + cls.consumer_key + str(cls.current_timestamp))\
+            str(cls.short_code) + cls.passkey + str(cls.current_timestamp))\
             .decode()
         result, status_code = oauth_generate_token(
             consumer_key=cls.consumer_key,
