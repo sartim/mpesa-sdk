d3db6bdf3ff9c2f371d6e0671a3c92b14ec04078
Tim <sarrtim@gmail.com>
2020-08-27 17:55:29 +0300

    Add lipa payment schema

--- mpesa_sdk/schemas.py
+++ mpesa_sdk/schemas.py
@@ -8,8 +8,22 @@ class AuthSchema(Schema):
     env = fields.Str(required=True, default="sandbox")
 
 
+class LipaPayment(Schema):
+    BusinessShortCode = fields.Int(required=True)
+    Password = fields.Str(required=True)
+    Amount = fields.Int(required=True)
+    PartyA = fields.Str(required=True)
+    PartyB = fields.Str(required=True)
+    PhoneNumber = fields.Str(required=True)
+    CallBackURL = fields.Str(required=True)
+    AccountReference = fields.Str(required=True)
+    TransactionDesc = fields.Str(required=True)
+    TransactionType = fields.Str(required=True, default="CustomerPayBillOnline")
+    Timestamp = fields.Str(required=True)
+
+
 class LipaQuery(Schema):
-    BusinessShortCode = fields.Str(required=True)
+    BusinessShortCode = fields.Int(required=True)
     Password = fields.Str(required=True)
     Timestamp = fields.Str(required=True)
     CheckoutRequestID = fields.Str(required=True)
