16ce9258004f0770df533252ccc80761d5c4f801
Tim <sarrtim@gmail.com>
2020-08-27 17:48:06 +0300

    Add lipa query schema

--- mpesa_sdk/schemas.py
+++ mpesa_sdk/schemas.py
@@ -6,3 +6,10 @@ class AuthSchema(Schema):
     consumer_secret = fields.Str(required=True)
     grant_type = fields.Str(required=True, default="client_credentials")
     env = fields.Str(required=True, default="sandbox")
+
+
+class LipaQuery(Schema):
+    BusinessShortCode = fields.Str(required=True)
+    Password = fields.Str(required=True)
+    Timestamp = fields.Str(required=True)
+    CheckoutRequestID = fields.Str(required=True)
